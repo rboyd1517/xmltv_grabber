31e70a4cd97933dac521293fcec563bcab3648fd
Ryan Boyd <rboyd1517@gmail.com>
2021-09-22 01:02:00 -0500

    Initial commit.

--- xmltv_grabber.py
+++ xmltv_grabber.py
@@ -12,11 +12,13 @@ import xml.etree.ElementTree as ElementTree
 
 TIMESPAN_HOURS = 6
 TIMESPAN_SECONDS = TIMESPAN_HOURS * 3600
+CHANNEL_FILE = 'channels.sql'
 
 qs = {'country': 'USA', 'headendId': 'lineupId', 'timespan': TIMESPAN_HOURS, 'postalCode': 66062}
 
 t_start = int(time.time() // 1800) * 1800
-t_end = t_start + 14 * 24 * 60 * 60
+# t_end = t_start + 14 * 24 * 60 * 60
+t_end = t_start + 1
 
 print(t_start, t_end)
 
@@ -31,9 +33,16 @@ for t in range(t_start, t_end, TIMESPAN_SECONDS):
     json_data = urllib.request.urlopen(url)
     data = json.load(json_data)
 
+    with open(CHANNEL_FILE, mode='w') as channel_file:
+        pass  # To erase any existing file
+
     for channel in data['channels']:
         raw_channel = float(channel['channelNo'])
+        underscore_channel = re.sub('\.', '_', channel['channelNo'])
         channel_num = f"{int(10000 + 100 * int(raw_channel) + 10 * (raw_channel % 1))}"
+        with open(CHANNEL_FILE, mode='a') as channel_file:
+            channel_file.write(f"update channel set xmltvid='{channel_num}' "
+                               f"where sourceid=1 and channum='{underscore_channel}';\n")
         for event in channel['events']:
             program = ElementTree.SubElement(xml_data, 'programme')
             program.set('channel', channel_num)
